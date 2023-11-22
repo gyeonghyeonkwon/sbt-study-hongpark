961cf0d5abe9af6a236a7583f1c92b95bfa21a96
gyeonghyeonkwon <kyanghyang12@naver.com>
2023-11-22 13:46:45 +0900

    1일차 완료

---
+++ src/main/java/com/ll/sbtstudy/SbtStudyApplication.java
@@ -0,0 +1,13 @@
+package com.ll.sbtstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbtStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbtStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/sbtstudy/SbtStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sbtstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbtStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
